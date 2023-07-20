40660db5add1b184b95c2a02d1061e64e77b1535
Admin <admin@MacBook-Pro-Admin.local>
2023-07-20 20:14:06 +0300

    add gendiff

---
+++ gendiff.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    return parser
+
+if __name__ == "__main__":
+    parser = main()
+    args = parser.parse_args()
+
+    parser.print_help()
